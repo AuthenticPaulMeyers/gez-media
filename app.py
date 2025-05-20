f383acf87af899046fd001dcfc9e08cb6c5a6df0
paul meyers <authenticpaulmeyers@gmail.com>
2025-05-20 23:30:48 +0200

    Modifications for deployments

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from website import create_app
+from website import create_app, db
 
 myapp=create_app()
 
 
 if __name__ == '__main__':
     # Run the Flask application
-    myapp.run()
+    with myapp.app_context():
+        # Create the database tables
+        db.create_all()
+        myapp.run(debug=True)
