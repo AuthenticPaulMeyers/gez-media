cdaacb15d2430907951a9cd87ecba64696d190ce
paul meyers <authenticpaulmeyers@gmail.com>
2025-05-20 22:56:11 +0200

    Modifications for deployment

--- website/__init__.py
+++ website/__init__.py
@@ -17,20 +17,12 @@ csrf = CSRFProtect()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 
-def create_app(test_config=None):
-    app = Flask(__name__, static_url_path='/static', instance_relative_config=True)
+def create_app():
+    app = Flask(__name__, static_url_path='/static')
 
     # create and configure the app
-    # Load environment variables from .env file
-    if test_config is None:
-        app.config.from_mapping(
-            SECRET_KEY = os.environ.get('SECRET_KEY'),
-            SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI'),
-            SQLALCHEMY_TRACK_MODIFICATIONS = False
-        )
-    else:
-        # Load the test configuration if provided
-        app.config.from_mapping(test_config)
+    app.config['SECRET_KEY'] = 'yugdugvucdiurhuheirhiuhiuhe37843gjbjhsdfBHHShVDHsD'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///media.db'
 
     # Initialize the database
     db.init_app(app)
